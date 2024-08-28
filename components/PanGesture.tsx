eb01f0060969f8a6b123a9b3555f4f085761346b
Shaon Ahmed <shaon.ahmed@hackberry.se>
2024-08-28 05:25:43 +0200

    Animated view now saves its last poistion

--- components/PanGesture.tsx
+++ components/PanGesture.tsx
@@ -7,7 +7,6 @@ import {
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
-  withSpring,
 } from "react-native-reanimated";
 
 interface GestureProps {
@@ -16,19 +15,24 @@ interface GestureProps {
 }
 
 export const GestureHandler = ({ width, height }: GestureProps) => {
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
+  const translateX = useSharedValue<number>(0);
+  const translateY = useSharedValue<number>(0);
+  const currentX = useSharedValue<number>(0);
+  const currentY = useSharedValue<number>(0);
 
   const pan = Gesture.Pan()
-    .onStart((event) => {})
-    .onChange((event) => {
-      translateX.value = event.translationX;
-      translateY.value = event.translationY;
+    .onBegin((event) => {
+      currentX.value = translateX.value;
+      currentY.value = translateY.value;
     })
-    .onFinalize(() => {
-      translateX.value = withSpring(0);
-      translateY.value = withSpring(0);
+    .onUpdate((event) => {
+      translateX.value = currentX.value + event.translationX;
+      translateY.value = currentY.value + event.translationY;
     });
+  // .onFinalize((event) => {
+  //   currentX.value = event.translationX;
+  //   currentY.value = event.translationY;
+  // });
 
   const style = useAnimatedStyle(() => {
     return {
